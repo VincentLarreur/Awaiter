4bfc35282677bc14ae41270d609b45550b662f11
Vincent Larreur <vincent.lrr@gmail.com>
2021-08-06 20:03:18 +0200

    :bug: upgrade to manifest v3 with alarms in background script

--- background.js
+++ background.js
@@ -1,33 +1,28 @@
-let date = new Date();
-
-chrome.storage.local.set({date: date.toISOString()}, function() {
-    console.log('first date is stored ' + date.toISOString());
-});
-
 const hour = 1000 * 60 * 60;
 const day = hour * 24;
 const week = day * 7;
 
-setBadge();
+chrome.storage.local.set({date: new Date().toISOString()});
 
-setInterval(setBadge, 1000);
+chrome.action.setBadgeBackgroundColor({color: "#E17754"});
 
-chrome.browserAction.setBadgeBackgroundColor({color: "#E17754"});
+chrome.alarms.create({ periodInMinutes: 60 });
 
-function setBadge() {
+chrome.alarms.onAlarm.addListener(() => {
+    console.log('lala');
     chrome.storage.local.get(['date'], function(result) {
         let now = new Date().getTime();
         distance = new Date(result.date).getTime() - now;
 
         const weeks = Math.floor(distance / (week));
-        chrome.browserAction.setBadgeText({text: 'None'});
+        chrome.action.setBadgeText({text: 'None'});
         if(weeks > 0) {
-            chrome.browserAction.setBadgeText({text: `${weeks}w`});
+            chrome.action.setBadgeText({text: `${weeks}w`});
         } else {
             const days = Math.floor((distance % (week)) / (day));
             if (days > 0) {
-                chrome.browserAction.setBadgeText({text: `${days}d`});
+                chrome.action.setBadgeText({text: `${days}d`});
             }
         }
     });
-}
+});
--- options/options.js
+++ options/options.js
@@ -13,7 +13,7 @@ const second = 1000;
 const minute = second * 60;
 const hour = minute * 60;
 const day = hour * 24;
-const weeks = day * 7;
+const week = day * 7;
 
 let headline = document.getElementById("headline");
 let countdown = document.getElementById("countdown");
@@ -26,8 +26,8 @@ x = setInterval(function() {
   let now = new Date().getTime();
   distance = new Date(input.value).getTime() - now;
 
-  document.getElementById("weeks").innerText = Math.floor(distance / (weeks));
-  document.getElementById("days").innerText = Math.floor((distance % (weeks)) / (day));
+  document.getElementById("weeks").innerText = Math.floor(distance / (week));
+  document.getElementById("days").innerText = Math.floor((distance % (week)) / (day));
   document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour));
   document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute));
   document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
@@ -36,9 +36,21 @@ x = setInterval(function() {
     headline.innerText = "GOAL REACHED !";
     countdown.style.display = "none";
     content.style.display = "block";
+
+    chrome.action.setBadgeText({text: 'None'});
   } else {
     headline.innerText = "REACHING YOUR GOAL IN";
     countdown.style.display = "block";
     content.style.display = "none";
+
+    const weeks = Math.floor(distance / (week));
+    if(weeks > 0) {
+        chrome.action.setBadgeText({text: `${weeks}w`});
+    } else {
+        const days = Math.floor((distance % (week)) / (day));
+        if (days > 0) {
+            chrome.action.setBadgeText({text: `${days}d`});
+        }
+    }
   }
 }, 250);
